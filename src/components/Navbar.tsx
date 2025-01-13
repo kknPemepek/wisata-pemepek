e36823bdbd17816f3bfd67c6715d45d71e315ae7
Adinmm <muhammadmuayyadin@gmail.com>
2025-01-13 23:44:34 +0800

    hidden mobilenav saat klik di luar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { SlLogin } from "react-icons/sl";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import logo from "../assets/LogoKKN.jpeg";
 
 const Navbar = () => {
@@ -11,24 +11,13 @@ const Navbar = () => {
   const [isHidden, setIsHidden] = useState(false);
   const [currentPath, setCurrentPath] = useState("");
   const [scrollTimeout, setScrollTimeout] = useState<number | null>(null);
+  const dropdownRef = useRef<HTMLDivElement>(null); // Ref untuk dropdown
 
   const itemNav = [
-    {
-      item: "Beranda",
-      to: "/wisata-pemepek/",
-    },
-    {
-      item: "Tentang ",
-      to: "/wisata-pemepek/tentang",
-    },
-    {
-      item: "FAQ",
-      to: "/wisata-pemepek/faq",
-    },
-    {
-      item: "Wisata",
-      to: "/wisata-pemepek/wisata",
-    },
+    { item: "Beranda", to: "/wisata-pemepek/" },
+    { item: "Tentang ", to: "/wisata-pemepek/tentang" },
+    { item: "FAQ", to: "/wisata-pemepek/faq" },
+    { item: "Wisata", to: "/wisata-pemepek/wisata" },
   ];
 
   const cekPath = () => {
@@ -44,7 +33,6 @@ const Navbar = () => {
   useEffect(() => {
     let lastScrollY = window.scrollY;
     cekPath();
-    console.log(currentPath);
 
     const handleScroll = () => {
       if (window.scrollY > lastScrollY) {
@@ -73,103 +61,102 @@ const Navbar = () => {
         clearTimeout(scrollTimeout);
       }
     };
-  }, [scrollTimeout, currentPath]);
+  }, [scrollTimeout]);
+
+ 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
-    <div
-      className={`flex items-center justify-between shadow-sm h-20 px-6 md:px-6 fixed w-full z-50 transition-transform duration-300 bg-white ${
-        isHidden ? "-translate-y-full" : "translate-y-0"
-      }`}
-    >
-      {/* Logo */}
+    <div>
       <div
-        onClick={() => navigate("/wisata-pemepek/")}
-        className="cursor-pointer"
+        className={`flex items-center justify-between shadow-sm h-20 px-6 md:px-6 fixed w-full z-50 transition-transform duration-300 bg-white ${
+          isHidden ? "-translate-y-full" : "translate-y-0"
+        }`}
       >
-        <img className="w-14 bg-red-300" src={logo} alt="" />
-      </div>
-
-      {/* Desktop Navigation */}
-      <div className="hidden md:block">
-        <ul className="flex space-x-4 md:space-x-6">
-          {itemNav.map((item, index) => (
-            <li
-              onClick={() => navigate(item.to)}
-              className={`cursor-pointer text-sm md:text-base hover:text-green-400 hover:scale-110 transition-transform ${
-                path === item.to
-                  ? "text-green-400"
-                  : currentPath === item.to
-                  ? "text-green-400"
-                  : ""
-              } `}
-              key={index}
-            >
-              {item.item}
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      {/* Action Buttons */}
-      <div className="hidden md:block">
-        <div className="flex items-center space-x-4 md:space-x-6 ">
-          {/* Login Button */}
-          <div
-            className="flex items-center space-x-2 cursor-pointer hover:text-green-400 hover:scale-110 transition-transform"
-            onClick={() => navigate("/login")}
-          >
-            <SlLogin className="text-lg md:text-xl" />
-            <p className="text-sm md:text-base">Login</p>
-          </div>
+        <div onClick={() => navigate("/wisata-pemepek/")} className="cursor-pointer">
+          <img className="w-14 bg-red-300" src={logo} alt="Logo" />
         </div>
-      </div>
 
-      {/* Mobile Navigation */}
-      <div className="md:hidden relative ">
-        <button
-          className="text-xl cursor-pointer focus:outline-none"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          <RxHamburgerMenu className="text-xl hover:text-green-400 hover:scale-110 transition-transform" />
-        </button>
-        <div
-          className={`absolute right-0 mt-2 bg-white border rounded-lg shadow-md w-48 transition-all duration-300 ease-in-out transform ${
-            menuOpen
-              ? "scale-100 opacity-100 translate-y-0 pointer-events-auto"
-              : "scale-90 opacity-0 -translate-y-2 pointer-events-none"
-          }`}
-        >
-          <ul>
+        <div className="hidden md:block">
+          <ul className="flex space-x-4 md:space-x-6">
             {itemNav.map((item, index) => (
               <li
-                onClick={() => {
-                  navigate(item.to);
-                  setMenuOpen(false);
-                }}
-                className={`cursor-pointer px-4 py-2 hover:bg-green-50 hover:text-green-400 transition-colors duration-300 ease-in-out ${
-                  path === item.to
-                    ? "text-green-400"
-                    : currentPath === item.to
-                    ? "text-green-400"
-                    : ""
+                onClick={() => navigate(item.to)}
+                className={`cursor-pointer text-sm md:text-base hover:text-green-400 hover:scale-110 transition-transform ${
+                  path === item.to || currentPath === item.to ? "text-green-400" : ""
                 }`}
                 key={index}
               >
-                <p className="hover:scale-110 transition-transform duration-300 ease-in-out text-sm md:text-base">
-                  {item.item}
-                </p>
+                {item.item}
               </li>
             ))}
-            <li className="cursor-pointer px-4 py-2 hover:bg-green-50 hover:text-green-400 transition-colors duration-300 ease-in-out">
-              <div
-                className="flex items-center space-x-2 cursor-pointer hover:text-green-400 hover:scale-110 transition-transform duration-300 ease-in-out"
-                onClick={() => navigate("/login")}
-              >
-                <SlLogin className="text-sm md:text-base" />
-                <p className="text-sm md:text-base">Login</p>
-              </div>
-            </li>
           </ul>
+        </div>
+
+        <div className="hidden md:block">
+          <div className="flex items-center space-x-4 md:space-x-6">
+            <div
+              className="flex items-center space-x-2 cursor-pointer hover:text-green-400 hover:scale-110 transition-transform"
+              onClick={() => navigate("/login")}
+            >
+              <SlLogin className="text-lg md:text-xl" />
+              <p className="text-sm md:text-base">Login</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Mobile Navigation */}
+        <div className="md:hidden relative" ref={dropdownRef}>
+          <button
+            className="text-xl cursor-pointer focus:outline-none"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <RxHamburgerMenu className="text-xl hover:text-green-400 hover:scale-110 transition-transform" />
+          </button>
+          <div
+            className={`absolute right-0 mt-2 bg-white border rounded-lg shadow-md w-48 transition-all duration-300 ease-in-out transform ${
+              menuOpen
+                ? "scale-100 opacity-100 translate-y-0 pointer-events-auto"
+                : "scale-90 opacity-0 -translate-y-2 pointer-events-none"
+            }`}
+          >
+            <ul>
+              {itemNav.map((item, index) => (
+                <li
+                  onClick={() => {
+                    navigate(item.to);
+                    setMenuOpen(false);
+                  }}
+                  className={`cursor-pointer px-4 py-2 hover:bg-green-50 hover:text-green-400 transition-colors duration-300 ease-in-out ${
+                    path === item.to || currentPath === item.to ? "text-green-400" : ""
+                  }`}
+                  key={index}
+                >
+                  {item.item}
+                </li>
+              ))}
+              <li className="cursor-pointer px-4 py-2 hover:bg-green-50 hover:text-green-400 transition-colors duration-300 ease-in-out">
+                <div
+                  className="flex items-center space-x-2 cursor-pointer hover:text-green-400 hover:scale-110 transition-transform"
+                  onClick={() => navigate("/login")}
+                >
+                  <SlLogin className="text-sm md:text-base" />
+                  <p className="text-sm md:text-base">Login</p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
